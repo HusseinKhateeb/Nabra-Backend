046b01d13e0f30c2b0fac8d67cc112e547383c3e
HusseinKhateeb <202201626@bethlehem.edu>
2026-03-06 13:04:06 +0200

    feat: update multipart file upload settings in application.yml and add error handling routes in SecurityConfig

--- src/main/java/com/nabra/backend/config/SecurityConfig.java
+++ src/main/java/com/nabra/backend/config/SecurityConfig.java
@@ -79,6 +79,7 @@ public class SecurityConfig {
             .requestMatchers(
                 "/api/v1/auth/**",
                 "/api/v1/auth/google",
+              "/error",
                 "/v3/api-docs/**",
                 "/swagger-ui/**",
                 "/swagger-ui.html",
